1c0ca352d4911b100154464f2a68b77bc91b9d3b
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-30 18:22:07 +0530

    User Auth And review(pending)

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,14 +1,22 @@
 const express = require("express");
-const { getAllProducts, createProduct,updateProduct, deleteProduct, getSingleProduct } = require("../controllers/productController");
+const { getAllProducts, createProduct,updateProduct, deleteProduct, getSingleProduct, createProductReview,getProductReviews , deleteReview } = require("../controllers/productController");
 const router = express.Router();
 const {isAuthenticatedUser , authorizeRoles } = require("../middleware/auth")
 
 router.route("/products").get(getAllProducts)
-router.route("/product/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct)
-router.route("/product/:id")
+router.route("/admin/product/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct)
+router.route("/admin/product/:id")
 .put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct)
-.delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct)
-.get(getSingleProduct)
+.delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct);
 
+router.route("/product/:id").get(getSingleProduct)
+
+
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 
 module.exports= router
